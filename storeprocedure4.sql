72955710ed54f41ffcd43d525a1b548888a85a23
pranitamane25 <pranitamane2506@gmail.com>
2025-07-26 12:07:51 +0530

    store procedure4

--- storeprocedure4.sql
+++ storeprocedure4.sql
@@ -1,8 +1,8 @@
+
 -- 4. Creating a Stored Procedure for Applying a Discount Code
 
 DELIMITER //
-
-CREATE PROCEDURE sp_ApplyDiscount (IN p_orderid int,IN p_discountcode varchar(100))
+CREATE PROCEDURE SP_ApplyDiscount( IN p_order_id varchar(100),IN p_discount_code varchar(500))
 
 BEGIN
 
@@ -11,15 +11,21 @@ DECLARE total decimal(10,2);
 
 -- Retrieve discount percentage from the discount_codes table
 
-select discount_percentage INTO discount FROM discount_codes where code=p_discountcode AND NOW() between start_date AND end_date;
-select total_amount INTO total FROM orders where id=p_orderid;
+select discount_percentage INTO discount FROM discount_code WHERE code=p_discount_code and NOW() BETWEEN start_date and end_date;
 
-set total=total-(total*discount/100);
-update orders set total_amount=total where id=p_orderid;
+ -- If discount exists, apply it
+ IF discount IS NOT NULL then
+select total_amount INTO total FROM orders WHERE id=p_order_id;
 
-END//
+-- Apply discount to total
+SET total=total-(total*discount/100);
+
+ -- Update order total
+UPDATE orders SET total_amount=total WHERE id=p_order_id;
+ELSE
+
+ SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT "INVALID or expired discount code" ;
+ 
+END IF ;
+END ;
 DELIMITER ;
--- drop procedure sp_ApplyDiscount;
-call sp_ApplyDiscount(20,"SUMMER23");
-
-select * from orders;
